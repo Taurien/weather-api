99c7df930edbd80a1351e8e946dd391afa749bdb
Taurien <michel.a.c474@gmail.com>
2022-06-28 01:23:34 -0500

    fixed form behavior

--- src/components/Home/Form/WeatherForm.jsx
+++ src/components/Home/Form/WeatherForm.jsx
@@ -5,16 +5,16 @@ import magnifying from '../../../assets/magnifying-glass.svg'
 const WeatherForm = ({ handleCity, handleFetch }) => {
     return (
         <form action=''
-        className='overflow-hidden rounded-full w-8/12 desktop:w-2/5 box-shdw'
+        className='w-8/12 desktop:w-2/5 h-8'
         onSubmit={e => handleFetch(e)}>
             <input
                 type="text"
                 placeholder='City Name'
                 onChange={({ target }) => handleCity(target)}
-                className='w-8/12 px-4 py-1 outline-none'
+                className='w-8/12 px-4 py-1 outline-none rounded-l-full'
             />
-            <div className='inline-flex items-center justify-center w-4/12 px-4 py-1 outline-none bg-gray-200'>
-                <input type="submit" value='Search' className='outline-none mr-1'/>
+            <div className=' inline-flex items-center justify-center w-4/12 px-4 py-1 outline-none bg-gray-200 rounded-r-full'>
+                <input type="submit" value='Search' className='outline-none mr-1 bg-transparent'/>
                 <img className='w-4' src={magnifying} alt="" />
             </div>
         </form>
